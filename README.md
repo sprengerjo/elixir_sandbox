2a341f64575d70e87ffcff7b6b6da59630f4e654
sprengerjo <jonas.sprenger@huf-group.com>
2018-12-19 20:37:53 +0100

    add: credo dependency and fix moduledoc issue

--- README.md
+++ README.md
@@ -1,21 +1,6 @@
-# KV
+# elixir_sandbox
 
-**TODO: Add description**
+**Katas and other stuff goes here...**
 
-## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `elixir_sanbox` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:elixir_sanbox, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/elixir_sanbox](https://hexdocs.pm/elixir_sanbox).
 
--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,4 +1,12 @@
-defmodule BOWLING do
+defmodule Bowling do
+  @moduledoc """
+    Provides bowling calculator function.
+
+   ## Examples
+       iex> rolls = Enum.map 1..12, fn (_) -> 10 end
+       iex> Bowling.score(rolls)
+       300
+  """
 
   def score(rolls) do
     Enum.sum(rolls) + bonus(rolls)
@@ -9,7 +17,7 @@ defmodule BOWLING do
       [] -> 0
       [_, _, _] -> 0
       [10 | tail] -> Enum.sum(Enum.take tail, 2) + bonus tail
-      [head, second | tail] when head + second == 10 -> Enum.at tail, 0 + bonus tail
+      [head, second | tail] when head + second == 10 -> List.first(tail) + bonus tail
       [_, _ | tail] -> bonus tail
     end
   end
